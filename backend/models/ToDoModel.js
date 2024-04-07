39db0fe0a60ccc8d512146733796bc5a6a7049ca
dona-murmu <109920996+coding-junction@users.noreply.github.com>
2024-04-07 22:38:18 +0530

    first comit

---
+++ backend/models/ToDoModel.js
@@ -0,0 +1,12 @@
+// todomodel.js
+
+const mongoose = require('mongoose')
+
+const todoSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        require: true
+    }
+})
+
+module.exports = mongoose.model('ToDo', todoSchema)
